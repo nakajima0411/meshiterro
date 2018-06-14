553378c7d1e9ab7d50385f5b61a6b0d04ba0ec08
nakajima0411 <sphere.system0411@gmail.com>
2018-06-14 20:08:34 +0900

    Finish 5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '50e2c0da6986805792443053b267b06d2c7141270cd38044a39868d0ebabcf0156ec89736de416dfceb8a4e920d595e0945f3092e471396cb8c2079db25f76ac'
